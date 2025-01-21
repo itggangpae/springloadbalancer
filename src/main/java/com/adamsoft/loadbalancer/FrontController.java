5e185c400cf798f957cc7c37393019a54ecdf426
itggangpae <ggangpae1@gmail.com>
2025-01-21 15:49:21 +0900

    init

---
+++ src/main/java/com/adamsoft/loadbalancer/FrontController.java
@@ -0,0 +1,16 @@
+package com.adamsoft.loadbalancer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class FrontController {
+
+    @GetMapping("/")
+    public String index() {
+        log.info("로드밸런싱");
+        return "index";
+    }
+}
